a697a68db0e9271150166004161692db8d4933f6
Luke Dinh <dinhphuoclockt@gmail.com>
2024-10-18 11:31:21 +0700

    updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,17 +2,12 @@ import streamlit as st
 import replicate
 import os
 
-# Instruct Prompt
-with open("instruct_prompt.txt", "r") as f:
-    instruct_prompt = f.read()
-
 # App title
 st.set_page_config(page_title="🦙💬 Assistant Chatbot")
 
 # Replicate Credentials
 with st.sidebar:
-    st.title('🦙💬 Assistant Chatbot')
-    st.write('This chatbot is created using the open-source Llama 2 LLM model from Meta.')
+    st.title('🦙💬 Llama 2 Chatbot')
     if 'REPLICATE_API_TOKEN' in st.secrets:
         st.success('API key already provided!', icon='✅')
         replicate_api = st.secrets['REPLICATE_API_TOKEN']
@@ -22,21 +17,25 @@ with st.sidebar:
             st.warning('Please enter your credentials!', icon='⚠️')
         else:
             st.success('Proceed to entering your prompt message!', icon='👉')
-    os.environ['REPLICATE_API_TOKEN'] = replicate_api
 
     st.subheader('Models and parameters')
-    selected_model = st.sidebar.selectbox('Choose a Llama2 model', ['Llama2-7B', 'Llama2-13B'], key='selected_model')
+    selected_model = st.sidebar.selectbox('Choose a Llama2 model', ['Llama2-7B', 'Llama2-13B', 'Llama2-70B'], key='selected_model')
     if selected_model == 'Llama2-7B':
-        llm = 'a16z-infra/llama7b-v2-chat:4f0a4744c7295c024a1de15e1a63c880d3da035fa1f49bfd344fe076074c8eea'
+        llm = "meta/llama-2-7b-chat"
     elif selected_model == 'Llama2-13B':
-        llm = 'a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5'
-    temperature = st.sidebar.slider('temperature', min_value=0.01, max_value=1.0, value=0.1, step=0.01)
+        llm = "meta/llama-2-13b-chat"
+    else:
+        llm = "meta/llama-2-70b-chat"
+    
+    temperature = st.sidebar.slider('temperature', min_value=0.01, max_value=5.0, value=0.1, step=0.01)
     top_p = st.sidebar.slider('top_p', min_value=0.01, max_value=1.0, value=0.9, step=0.01)
-    max_length = st.sidebar.slider('max_length', min_value=20, max_value=80, value=50, step=5)
+    max_length = st.sidebar.slider('max_length', min_value=64, max_value=4096, value=512, step=8)
+    
+os.environ['REPLICATE_API_TOKEN'] = replicate_api
 
 # Store LLM generated responses
 if "messages" not in st.session_state.keys():
-    st.session_state.messages = [{"role": "assistant", "content": "Hi Dad, I am Choi, your son. How can I help you today?"}]
+    st.session_state.messages = [{"role": "assistant", "content": "Hi Dad, I'm your son, Choi. How can I help you today?"}]
 
 # Display or clear chat messages
 for message in st.session_state.messages:
@@ -44,8 +43,12 @@ for message in st.session_state.messages:
         st.write(message["content"])
 
 def clear_chat_history():
-    st.session_state.messages = [{"role": "assistant", "content": "Hi Dad, I am Choi, your spn. How can I help you today?"}]
+    st.session_state.messages = [{"role": "assistant", "content": "Hi Dad, I'm your son, Choi. How can I help you today?"}]
 st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
+
+# Function for generating LLaMA2 response
+with open("instruct_prompt.txt", "r") as f:
+    instruct_prompt = f.read()
 
 def generate_llama2_response(prompt_input):
     string_dialogue = instruct_prompt
@@ -54,7 +57,7 @@ def generate_llama2_response(prompt_input):
             string_dialogue += "User: " + dict_message["content"] + "\n\n"
         else:
             string_dialogue += "Assistant: " + dict_message["content"] + "\n\n"
-    output = replicate.run('a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5', 
+    output = replicate.run(llm, 
                            input={"prompt": f"{string_dialogue} {prompt_input} Assistant: ",
                                   "temperature":temperature, "top_p":top_p, "max_length":max_length, "repetition_penalty":1})
     return output
